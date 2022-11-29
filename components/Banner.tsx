2998936751e44f83b8a874c208de17be804b9448
Jaco-Miras <miras.jaco@gmail.com>
2022-11-29 22:45:51 +0800

    COMMIT

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -21,7 +21,7 @@ function Banner({ netflixOriginals }: Props) {
   console.log(movie);
   return (
     <div
-      className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[85vh] lg:justify-end
+      className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[90vh] lg:justify-end
     lg:pb-12"
     >
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
@@ -35,7 +35,7 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl ">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl justify-center">
         {movie?.overview}
       </p>
 
